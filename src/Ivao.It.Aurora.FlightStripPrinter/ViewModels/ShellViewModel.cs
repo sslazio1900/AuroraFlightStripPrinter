991a698f9abbcc25451fe385715c642ceea2a2e8
Innocenti Emiliano <emiliano@emilianoinnocenti.com>
2022-11-10 09:25:17 +0100

    App Version

--- src/Ivao.It.Aurora.FlightStripPrinter/ViewModels/ShellViewModel.cs
+++ src/Ivao.It.Aurora.FlightStripPrinter/ViewModels/ShellViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Caliburn.Micro;
+using System.Reflection;
 
 namespace Ivao.It.Aurora.FlightStripPrinter.ViewModels
 {
     public class ShellViewModel : Conductor<object>, IViewModel
     {
+        public string Version => $"v{Assembly.GetExecutingAssembly().GetName().Version!.Major}.{Assembly.GetExecutingAssembly().GetName().Version!.Minor}.{Assembly.GetExecutingAssembly().GetName().Version!.Build}";
+
         protected override async void OnViewLoaded(object view)
         {
             base.OnViewLoaded(view);
