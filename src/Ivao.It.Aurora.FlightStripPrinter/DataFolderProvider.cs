9fffa62b26aad47d0145f3721ac51320c3f48c0f
Innocenti Emiliano <emiliano@emilianoinnocenti.com>
2025-02-23 19:18:55 +0100

    New templates + debug templates folder

--- src/Ivao.It.Aurora.FlightStripPrinter/DataFolderProvider.cs
+++ src/Ivao.It.Aurora.FlightStripPrinter/DataFolderProvider.cs
@@ -1,19 +1,32 @@
 ﻿ using System;
 using System.IO;
+using System.Reflection;
 
 namespace Ivao.It.Aurora.FlightStripPrinter;
 
 internal static class DataFolderProvider
 {
     private const string SettingsFileName = "settings.yml";
+    private const string StripsFolderName = "Strips";
+    private const string TempaltesFolderName = "Templates";
+    private const string LogsFolderName = "Logs";
+
+
 
     private static string GetRootFolder()
         => Path.Combine(
             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
             @"IVAO Italy\Aurora Flight Strip Printer");
-
+    
     public static string SettingsFile => Path.Combine(GetRootFolder(), SettingsFileName);
-    public static string GetStripsFolder() => Path.Combine(GetRootFolder(), "Strips");
-    public static string GetTemplatesFolder() => Path.Combine(GetRootFolder(), "Templates");
-    public static string GetLogsFolder() => Path.Combine(GetRootFolder(), "Logs");
+    public static string GetStripsFolder() => Path.Combine(GetRootFolder(), StripsFolderName);
+    public static string GetTemplatesFolder()
+#if DEBUG
+        => Path.Combine(Environment.CurrentDirectory, TempaltesFolderName);
+
+#else
+        => Path.Combine(GetRootFolder(), TempaltesFolderName);
+#endif
+
+    public static string GetLogsFolder() => Path.Combine(GetRootFolder(), LogsFolderName);
 }
